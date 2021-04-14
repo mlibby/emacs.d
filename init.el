e88951bee3a6daebda12af5782d4c5ac724c95eb
Michael C. Libby <m@mlibby.com>
2021-04-13 21:53:14 -0500

    add pug-mode package

--- init.el
+++ init.el
@@ -11,12 +11,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (magit elpy)))
  '(ansi-color-faces-vector
    [default default default italic underline success warning error])
  '(ansi-color-names-vector
    ["#2e3436" "#a40000" "#4e9a06" "#c4a000" "#204a87" "#5c3566" "#729fcf" "#eeeeec"])
- '(custom-enabled-themes '(wheatgrass)))
+ '(custom-enabled-themes '(wheatgrass))
+ '(package-selected-packages '(pug-mode magit elpy)))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
