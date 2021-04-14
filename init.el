4609c891395795c16261c73927d1beb68d673e8b
Michael C. Libby <m@mlibby.com>
2021-04-14 08:32:37 -0500

    force install of missing packages

--- init.el
+++ init.el
@@ -6,6 +6,17 @@
 (when (not package-archive-contents)
   (package-refresh-contents))
 
+(defvar my-packages
+  '(magit
+    elpy
+    pug-mode
+    ))
+
+(mapc #'(lambda (package)
+	  (unless (package-installed-p package)
+	    (package-install package)))
+      my-packages)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -16,7 +27,7 @@
  '(ansi-color-names-vector
    ["#2e3436" "#a40000" "#4e9a06" "#c4a000" "#204a87" "#5c3566" "#729fcf" "#eeeeec"])
  '(custom-enabled-themes '(wheatgrass))
- '(package-selected-packages '(pug-mode magit elpy)))
+ '(package-selected-packages '(pug-mode elpy magit)))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -24,3 +35,5 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:family "Fira Code" :foundry "outline" :slant normal :weight normal :height 158 :width normal)))))
+
+(elpy-enable)
